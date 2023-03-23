c18c72158e29eb0f7339762c89ffd9e88145eb4f
iliacodes <6ac177ed@protonmail.com>
2023-03-22 17:52:56 -0700

    fixed type in hardhat docs. deploying now.

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
-  netowrks: {
+  networks: {
     goerli: {
       url: process.env.QUICKNODE_API_KEY_URL,
       accounts: [process.env.GOERLI_PRIVATE_KEY],
