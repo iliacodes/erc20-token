ef33907b279c63bc2b55c3867d5acd4845e7de67
iliacodes <6ac177ed@protonmail.com>
2023-03-22 17:48:31 -0700

    added to hardhat.config.js for deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  netowrks: {
+    goerli: {
+      url: process.env.QUICKNODE_API_KEY_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.QUICKNODE_API_KEY_URL,
+      accounts: [process.env.MAINNET_PRIVATE_KEY],
+    }
+  }
 };
